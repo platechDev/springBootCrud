0874da2f570933154cc876160a31592c50ffc228
axlMax <axlyeyo2000@gmail.com>
2024-08-11 17:37:30 -0500

    feature/create tbl_student

--- src/main/java/com/example/springBootCrud/entity/Student.java
+++ src/main/java/com/example/springBootCrud/entity/Student.java
@@ -1,5 +1,22 @@
 package com.example.springBootCrud.entity;
 
-public class Student {
+import lombok.Data;
+import jakarta.persistence.*;
 
+
+@Data
+@Entity
+@Table(name = "tbl_student")
+public class Student {
+	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String firstName;
+    
+    private String lastName;
+    
+    @Column(name = "email_address", unique = true)
+    private String email;
 }
